77309b966a8ad92f6c64fb90e2d1d3cfdbfe356e
pradeepkkkk <60964542+pradeepkkkk@users.noreply.github.com>
2020-02-13 17:25:32 +0530

    Add files via upload

---
+++ ex3.rb
@@ -0,0 +1,12 @@
+puts "I will now count my chickens:"
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Now I will count the eggs:"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts "is it true thet 3+2<5-7?"
+puts "How about some more."
+puts "Oh, that's why it's false."
+puts "3 + 2 < 5 - 7"
+puts "is it greater? {5 > -2)}"
+puts "is it greater or equal? #{5 >= -2}"
+puts "is it less or equal? #{5 <= -2}"
